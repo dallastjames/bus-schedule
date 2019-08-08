19516b0e45ffad47e89bb503592264ef37b92554
Dallas James <dallastjames@gmail.com>
2019-08-08 00:22:24 -0600

    refactor http tests

--- src/app/core/services/routes.service.spec.ts
+++ src/app/core/services/routes.service.spec.ts
@@ -4,6 +4,7 @@ import {
 } from '@angular/common/http/testing';
 import { RoutesService } from './routes.service';
 import { TestBed } from '@angular/core/testing';
+import { environment } from '@bus/env';
 
 describe('RoutesService', () => {
   let service: RoutesService;
@@ -22,62 +23,78 @@ describe('RoutesService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('refresh', () => {
+  describe('loadAllRoutes', () => {
     it('loads the data for the agency', () => {
-      expect(true).toEqual(true);
-      //   service.refresh('umn-twin')
-      //   expect(connection.request.url).toEqual(
-      //     'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=umn-twin'
-      //   );
-      //   expect(connection.request.method).toEqual(RequestMethod.Get);
+      service.loadAllRoutes('sf-muni').subscribe();
+
+      const url = `${environment.dataServiceUrl}?command=routeList&a=sf-muni`;
+      const req = httpController.expectOne(url);
+      expect(req).toBeTruthy();
+
+      req.flush(null);
     });
 
     it('emits new data', () => {
-      expect(true).toEqual(true);
-      //   let body = '<body>';
-      //   body += route('1', '1-California');
-      //   body += route('1AX', '1AX-California A Express');
-      //   body += route('1BX', '1BX-California B Express');
-      //   body += route('2', '2-Clement');
-      //   body += route('3', '3-Jackson');
-      //   body += route('5', '5-Fulton');
-      //   body += route('5R', '5R-Fulton Rapid');
-      //   body += '</body>';
-      //   let result: any;
-      //   service.data.subscribe(r => (result = r));
-      //   service.refresh('sf-muni');
-      //   expect(result).toEqual([
-      //     { tag: '1', title: '1-California' },
-      //     { tag: '1AX', title: '1AX-California A Express' },
-      //     { tag: '1BX', title: '1BX-California B Express' },
-      //     { tag: '2', title: '2-Clement' },
-      //     { tag: '3', title: '3-Jackson' },
-      //     { tag: '5', title: '5-Fulton' },
-      //     { tag: '5R', title: '5R-Fulton Rapid' }
-      //   ]);
+      let body = '<body>';
+      body += route('1', '1-California');
+      body += route('1AX', '1AX-California A Express');
+      body += route('1BX', '1BX-California B Express');
+      body += route('2', '2-Clement');
+      body += route('3', '3-Jackson');
+      body += route('5', '5-Fulton');
+      body += route('5R', '5R-Fulton Rapid');
+      body += '</body>';
+      service.loadAllRoutes('sf-muni').subscribe(res => {
+        expect(res).toEqual([
+          { tag: '1', title: '1-California' },
+          { tag: '1AX', title: '1AX-California A Express' },
+          { tag: '1BX', title: '1BX-California B Express' },
+          { tag: '2', title: '2-Clement' },
+          { tag: '3', title: '3-Jackson' },
+          { tag: '5', title: '5-Fulton' },
+          { tag: '5R', title: '5R-Fulton Rapid' }
+        ]);
+      });
+
+      const url = `${environment.dataServiceUrl}?command=routeList&a=sf-muni`;
+      const req = httpController.expectOne(url);
+
+      req.flush(body);
     });
 
     it('emits an array of one with a single route in the response', () => {
-      expect(true).toEqual(true);
-      //   let body = '<body>';
-      //   body += route('1BX', '1BX-California B Express');
-      //   body += '</body>';
-      //   let result: any;
-      //   service.data.subscribe(r => (result = r));
-      //   service.refresh('sf-muni');
-      //   expect(result).toEqual([
-      //     { tag: '1BX', title: '1BX-California B Express' }
-      //   ]);
+      let body = '<body>';
+      body += route('1BX', '1BX-California B Express');
+      body += '</body>';
+      service.loadAllRoutes('sf-muni').subscribe(res => {
+        expect(res).toEqual([
+          { tag: '1BX', title: '1BX-California B Express' }
+        ]);
+      });
+
+      const url = `${environment.dataServiceUrl}?command=routeList&a=sf-muni`;
+      const req = httpController.expectOne(url);
+
+      req.flush(body);
     });
 
     it('emits an empty array with no routes in the response', () => {
-      expect(true).toEqual(true);
-      //   let body = '<body><lastTime time="1499622357839" />';
-      //   body += '</body>';
-      //   let result: any;
-      //   service.data.subscribe(r => (result = r));
-      //   service.refresh('sf-muni');
-      //   expect(result).toEqual([]);
+      let body = '<body><lastTime time="1499622357839" />';
+      body += '</body>';
+      service.loadAllRoutes('sf-muni').subscribe(res => {
+        expect(res).toEqual({
+          body: {
+            lastTime: {
+              time: 1499622357839
+            }
+          }
+        });
+      });
+
+      const url = `${environment.dataServiceUrl}?command=routeList&a=sf-muni`;
+      const req = httpController.expectOne(url);
+
+      req.flush(body);
     });
   });
 
--- src/app/core/services/vehicle-locations.service.spec.ts
+++ src/app/core/services/vehicle-locations.service.spec.ts
@@ -4,6 +4,7 @@ import {
 } from '@angular/common/http/testing';
 import { VehicleLocationsService } from './vehicle-locations.service';
 import { TestBed } from '@angular/core/testing';
+import { environment } from '@bus/env';
 
 describe('VehicleLocationService', () => {
   let service: VehicleLocationsService;
@@ -22,137 +23,135 @@ describe('VehicleLocationService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('refresh', () => {
-    it('loads the data for the agency, defaulting to timestamp to zero', () => {
-      expect(true).toEqual(true);
-      //   let connection: MockConnection;
-      //   mockBackend.connections.subscribe(c => connection = c);
-      //   service.refresh('umn-twin');
-      //   expect(connection.request.url)
-      //     .toEqual('http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=umn-twin&t=0');
-      //   expect(connection.request.method).toEqual(RequestMethod.Get);
-    });
-
+  describe('getLatestVehicleLocations', () => {
     it('loads the data for the agency using the passed "since" ms timestamp', () => {
-      expect(true).toEqual(true);
-      //   let connection: MockConnection;
-      //   mockBackend.connections.subscribe(c => connection = c);
-      //   service.refresh('umn-twin', 1499622348293);
-      //   expect(connection.request.url)
-      //     .toEqual('http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=umn-twin&t=1499622348293');
-      //   expect(connection.request.method).toEqual(RequestMethod.Get);
+      service
+        .getLatestVehicleLocations('umn-twin', new Date(1499622348293))
+        .subscribe();
+      const url = `${environment.dataServiceUrl}?command=vehicleLocations&a=umn-twin&t=1499622348293`;
+      const req = httpController.expectOne(url);
+
+      expect(req).toBeTruthy();
+      req.flush(null);
     });
 
     it('emits new data', () => {
-      expect(true).toEqual(true);
-      //   let body = '<body><lastTime time="1499622357839" />';
-      //   body += location('42', 'wauk', 13.9950, -122.0050);
-      //   body += location('314', 'wauk', 73.9950, -122.0050);
-      //   body += location('73', 'mad', 75.223, -132.0050);
-      //   body += location('1138', 'wauk', 77.9950, -127.753);
-      //   body += '</body>';
-      //   let connection: MockConnection;
-      //   mockBackend.connections.subscribe(c => connection = c);
-      //   let result: any;
-      //   service.data.subscribe(r => result = r);
-      //   service.refresh('umn-twin', 1499622348293);
-      //   connection.mockRespond(new Response(new ResponseOptions({
-      //     status: 200,
-      //     body: body
-      //   })));
-      //   expect(result).toEqual({
-      //     lastTime: 1499622357839,
-      //     locations: [{
-      //       id: '42',
-      //       routeTag: 'wauk',
-      //       dirTag: '30___O_S10',
-      //       lat: '13.995',
-      //       lon: '-122.005',
-      //       secsSinceReport: '20',
-      //       predictable: 'true',
-      //       heading: '350',
-      //       speedKmHr: '0'
-      //     }, {
-      //       id: '314',
-      //       routeTag: 'wauk',
-      //       dirTag: '30___O_S10',
-      //       lat: '73.995',
-      //       lon: '-122.005',
-      //       secsSinceReport: '20',
-      //       predictable: 'true',
-      //       heading: '350',
-      //       speedKmHr: '0'
-      //     }, {
-      //       id: '73',
-      //       routeTag: 'mad',
-      //       dirTag: '30___O_S10',
-      //       lat: '75.223',
-      //       lon: '-132.005',
-      //       secsSinceReport: '20',
-      //       predictable: 'true',
-      //       heading: '350',
-      //       speedKmHr: '0'
-      //     }, {
-      //       id: '1138',
-      //       routeTag: 'wauk',
-      //       dirTag: '30___O_S10',
-      //       lat: '77.995',
-      //       lon: '-127.753',
-      //       secsSinceReport: '20',
-      //       predictable: 'true',
-      //       heading: '350',
-      //       speedKmHr: '0'
-      //     }]
-      //   });
+      let body = '<body><lastTime time="1499622357839" />';
+      body += location('42', 'wauk', 13.995, -122.005);
+      body += location('314', 'wauk', 73.995, -122.005);
+      body += location('73', 'mad', 75.223, -132.005);
+      body += location('1138', 'wauk', 77.995, -127.753);
+      body += '</body>';
+      service
+        .getLatestVehicleLocations('umn-twin', new Date(1499622348293))
+        .subscribe(res => {
+          expect(res).toEqual({
+            lastTime: 1499622357839,
+            locations: [
+              {
+                id: '42',
+                routeTag: 'wauk',
+                dirTag: '30___O_S10',
+                lat: '13.995',
+                lon: '-122.005',
+                secsSinceReport: '20',
+                predictable: 'true',
+                heading: '350',
+                speedKmHr: '0'
+              },
+              {
+                id: '314',
+                routeTag: 'wauk',
+                dirTag: '30___O_S10',
+                lat: '73.995',
+                lon: '-122.005',
+                secsSinceReport: '20',
+                predictable: 'true',
+                heading: '350',
+                speedKmHr: '0'
+              },
+              {
+                id: '73',
+                routeTag: 'mad',
+                dirTag: '30___O_S10',
+                lat: '75.223',
+                lon: '-132.005',
+                secsSinceReport: '20',
+                predictable: 'true',
+                heading: '350',
+                speedKmHr: '0'
+              },
+              {
+                id: '1138',
+                routeTag: 'wauk',
+                dirTag: '30___O_S10',
+                lat: '77.995',
+                lon: '-127.753',
+                secsSinceReport: '20',
+                predictable: 'true',
+                heading: '350',
+                speedKmHr: '0'
+              }
+            ]
+          });
+        });
+
+      const url = `${environment.dataServiceUrl}?command=vehicleLocations&a=umn-twin&t=1499622348293`;
+      const req = httpController.expectOne(url);
+
+      req.flush(body);
     });
 
     it('emits an array of one with a single vehicle in the response', () => {
-      expect(true).toEqual(true);
-      //   let body = '<body><lastTime time="1499622357839" />';
-      //   body += location('314', 'wauk', 73.9950, -122.0050);
-      //   body += '</body>';
-      //   let connection: MockConnection;
-      //   mockBackend.connections.subscribe(c => connection = c);
-      //   let result: any;
-      //   service.data.subscribe(r => result = r);
-      //   service.refresh('umn-twin', 1499622348293);
-      //   connection.mockRespond(new Response(new ResponseOptions({
-      //     status: 200,
-      //     body: body
-      //   })));
-      //   expect(result).toEqual({
-      //     lastTime: 1499622357839,
-      //     locations: [{
-      //       id: '314',
-      //       routeTag: 'wauk',
-      //       dirTag: '30___O_S10',
-      //       lat: '73.995',
-      //       lon: '-122.005',
-      //       secsSinceReport: '20',
-      //       predictable: 'true',
-      //       heading: '350',
-      //       speedKmHr: '0'
-      //     }]
-      //   });
+      let body = '<body><lastTime time="1499622357839" />';
+      body += location('314', 'wauk', 73.995, -122.005);
+      body += '</body>';
+      service
+        .getLatestVehicleLocations('umn-twin', new Date(1499622348293))
+        .subscribe(res => {
+          expect(res).toEqual({
+            lastTime: 1499622357839,
+            locations: [
+              {
+                id: '314',
+                routeTag: 'wauk',
+                dirTag: '30___O_S10',
+                lat: '73.995',
+                lon: '-122.005',
+                secsSinceReport: '20',
+                predictable: 'true',
+                heading: '350',
+                speedKmHr: '0'
+              }
+            ]
+          });
+        });
+
+      const url = `${environment.dataServiceUrl}?command=vehicleLocations&a=umn-twin&t=1499622348293`;
+      const req = httpController.expectOne(url);
+
+      req.flush(body);
     });
 
     it('emits an empty array with no vehicles in the response', () => {
-      expect(true).toEqual(true);
-      //   let body = '<body><lastTime time="1499622357839" />';
-      //   body += '</body>';
-      //   let connection: MockConnection;
-      //   mockBackend.connections.subscribe(c => connection = c);
-      //   let result: any;
-      //   service.data.subscribe(r => result = r);
-      //   service.refresh('umn-twin', 1499622348293);
-      //   connection.mockRespond(new Response(new ResponseOptions({
-      //     status: 200,
-      //     body: body
-      //   })));
-      //   expect(result).toEqual({
-      //     lastTime: 1499622357839,
-      //     locations: []
-      //   });
+      let body = '<body><lastTime time="1499622357839" />';
+      body += '</body>';
+      service
+        .getLatestVehicleLocations('umn-twin', new Date(1499622348293))
+        .subscribe(res => {
+          expect(res).toEqual({
+            body: {
+              lastTime: {
+                time: 1499622357839
+              }
+            }
+          });
+        });
+
+      const url = `${environment.dataServiceUrl}?command=vehicleLocations&a=umn-twin&t=1499622348293`;
+      const req = httpController.expectOne(url);
+
+      req.flush(body);
     });
   });
 
